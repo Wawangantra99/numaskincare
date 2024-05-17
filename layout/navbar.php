8f3d4e75a97af6920aafb719fe535bf1af5a385d
Wawangantra99 <wawangantra@gmail.com>
2024-05-17 16:12:33 +0700

    first commit

---
+++ layout/navbar.php
@@ -0,0 +1,65 @@
+<nav
+      class="navbar navbar-expand-lg navbar-light text-light"
+      style="background-color: #a8dede"
+    >
+      <div class="container-fluid">
+        <img src="../Logo/LOGO.png" width=170px" height=100px"
+        class="ms-4" alt="" />
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarScroll"
+          aria-controls="navbarScroll"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarScroll">
+          <ul
+            class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll"
+            style="--bs-scroll-height: 0px"
+          ></ul>
+          <!-- <form class="d-flex">
+            <ul
+              class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll"
+              style="--bs-scroll-height: 280px"
+            >
+              <li class="nav-item">
+                <a
+                  class="nav-link active text-white fs-6"
+                  aria-current="page"
+                  href="#Produk"
+                  >Product</a
+                >
+              </li>
+              <li class="nav-item">
+                <a
+                  class="nav-link active text-white fs-6"
+                  aria-current="page"
+                  href="#Chart"
+                  >Size Chart</a
+                >
+              </li>
+              <li class="nav-item">
+                <a
+                  class="nav-link active text-white fs-6"
+                  aria-current="page"
+                  href="#order"
+                  >Buy Now</a
+                >
+              </li>
+              <li class="nav-item me-5">
+                <a
+                  class="nav-link active text-white fs-6"
+                  aria-current="page"
+                  href="#footer"
+                  >About Us</a
+                >
+              </li>
+            </ul>
+          </form> -->
+        </div>
+      </div>
+    </nav>
---
+++ layout/ulasan.php
@@ -0,0 +1,12 @@
+<div class="ms-2 me-2 mt-4">
+    <h5 class="text-center" style="font-family: 'Poppins';">Telah di ulas oleh</h5>
+    <br>
+    <div class="scroll-container">
+        <img src="../Logo/News/1.png" width="170px" height="70px" />
+        <img src="../Logo/News/2.png" width="170px" height="70px" />
+        <img src="../Logo/News/3.png" width="170px" height="70px" />
+        <img src="../Logo/News/4.png" width="170px" height="70px" />
+        <img src="../Logo/News/5.png" width="170px" height="70px" />
+        <img src="../Logo/News/6.png" width="170px" height="70px" />
+    </div>
+</div>
